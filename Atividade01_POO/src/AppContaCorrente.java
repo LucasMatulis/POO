0461f8dfe8401d0255adaef2235f6767e8fc5b0c
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2023-02-23 10:08:20 -0300

    sad

--- Atividade01_POO/src/AppContaCorrente.java
+++ Atividade01_POO/src/AppContaCorrente.java
@@ -3,10 +3,14 @@ import javax.swing.*;
 public class AppContaCorrente {
     public static void main(String[] args) {
 
-        Agencia agencia[]=new Agencia[10];
-        ContaCorrente contaCorrente[]= new ContaCorrente[10];
-        String nAgencia="";
-        String nContacorrente="";
+        int n=Integer.parseInt(JOptionPane.showInputDialog("Digite o numero de contas"));
+
+        char opcao;
+
+        Agencia agencia[]=new Agencia[n];
+        ContaCorrente contaCorrente[]= new ContaCorrente[n];
+        String nAgencia;
+        String nContacorrente;
         boolean acesso=false;
 
         for(int i=0;i<agencia.length;i++){
@@ -27,14 +31,5 @@ public class AppContaCorrente {
         nAgencia=JOptionPane.showInputDialog("Digite o numero da Agencia");
         nContacorrente=JOptionPane.showInputDialog("Digite o numero da Conta");
 
-        for (int i=0;i<agencia.length;i++){
-
-            if(nAgencia==agencia[i].getIdAgencia() && nContacorrente==contaCorrente[i].getNumeroConta()){
-
-                acesso=true;
-
-            }
-
-        }
     }
 }
