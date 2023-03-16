9d20f7e76ce27c4d6f6107cd9a29359e30730c12
LucasMatulis <lucas.matulis@gmail.com>
2023-03-16 16:12:29 -0300

    asd

---
+++ ADO02/src/Aluno.java
@@ -0,0 +1,8 @@
+public class Aluno extends Pessoa{
+
+    public void email(){
+
+        System.out.println("Solicitando a presença do aluno na secretaria");
+
+    }
+}
---
+++ ADO02/src/Professor.java
@@ -0,0 +1,8 @@
+public class Professor extends Pessoa{
+
+    public void email(){
+
+        System.out.println("Informando período de férias da instituição para o professor");
+
+    }
+}
